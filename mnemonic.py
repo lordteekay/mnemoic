c0eea3989511af09fcea778a46099cb571202c95
lordteekay <tokunboad@gmail.com>
2024-09-04 20:13:32 +0100

    I generated the entropy binary

--- mnemonic.py
+++ mnemonic.py
@@ -8,3 +8,10 @@ def generate_entropy(entropy_bits):
 
 entropy_generated = generate_entropy(128)
 print(f"Entropy:{entropy_generated}")
+
+def add_checksum(entropy_hex):
+    entropy_bin = bin(int(entropy_hex,16))[2:].zfill(len(entropy_hex)*4)
+    print(entropy_bin)
+    print(f"entropy_bin_len:{len(entropy_bin)}")
+
+add_checksum(entropy_generated)
